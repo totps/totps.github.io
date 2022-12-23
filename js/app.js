e3d916c7af7ac62e2f5b050e56bce1ec5a87bdbe
Big Trust <admin@example.com>
2022-12-23 22:30:32 +0800

    init

--- js/app.js
+++ js/app.js
@@ -29,12 +29,17 @@ function parseURLSearch(search) {
 new Vue({
   el: '#app',
   data: {
-    secret_key: 'JBSWY3DPEHPK3PXP',
+    secret_key: '',
     digits: 6,
     period: 30,
     updatingIn: 30,
     token: null,
     clipboardButton: null,
+    username: '',
+    password: '',
+    SecretKeyTitle: 'Your Secret Key',
+    hasError: false,
+    activeClass: "active"
   },
 
   mounted: function () {
@@ -59,7 +64,7 @@ new Vue({
         period: this.period,
         secret: OTPAuth.Secret.fromB32(stripSpaces(this.secret_key)),
       });
-    }
+    },
   },
 
   methods: {
@@ -89,6 +94,33 @@ new Vue({
       if (queryParams.period) {
         this.period = queryParams.period;
       }
+    },
+
+    updateSecret: function() {
+        console.log(this.secret_key)
+        if(this.secret_key.length !=16){
+            this.hasError = true
+            this.SecretKeyTitle = "Your Secret Key (Length must be 16)"
+        }else{
+            this.hasError = false
+            this.SecretKeyTitle = "Your Secret Key"
+        }        
+    },
+
+    generateUsername: function(){
+        var randomUsername = faker.internet.userName()
+        this.username = randomUsername;
+    },
+
+    generatePassword: function(){
+        var pasArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','_','-','$','%','&','@','+','!'];
+        var password = '';
+        var pasArrLen = pasArr.length;
+        for (var i=0; i<12; i++){
+            var x = Math.floor(Math.random()*pasArrLen);
+            password += pasArr[x];
+        }
+        this.password = password;
     }
   }
 });
